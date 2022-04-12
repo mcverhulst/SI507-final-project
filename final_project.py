1f411fc953033ce75de63f3b4bf1171f01e44176
mcverhulst <verhulst@umich.edu>
2022-04-11 21:43:56 -0400

    initial commit

---
+++ final_project.py
@@ -0,0 +1,26 @@
+# Final Project, SI 507
+# Author: Mike VerHulst
+# 
+
+import requests
+import pandas as pd
+import final_utils as utils
+
+base_url = 'https://statsapi.web.nhl.com/api/v1/'
+
+# team_id = 15
+# season = "20212022"
+# r = requests.get("https://statsapi.web.nhl.com/api/v1/teams/" + str(team_id) + "?expand=team.roster&season=" + season)
+# team_data = r.json()
+
+# getting a list of all active teams
+teams = requests.get('https://statsapi.web.nhl.com/api/v1/teams').json()['teams']
+# print(teams)
+
+# getting rosters for all active teams
+teams_rosters = []
+for team in teams:
+    team_id = team['id']
+    teams_rosters.append(requests.get(f"{base_url}teams/{team_id}?expand=team.roster&season=20212022").json()['teams'][0])
+
+utils.write_json('20212022_rosters.json' , teams_rosters)
---
+++ final_utils.py
@@ -0,0 +1,115 @@
+import csv
+import json
+import requests
+
+
+def get_nhl_data(url, params=None, timeout=10):
+    """Returns a response object decoded into a dictionary. If query string < params > are
+    provided the response object body is returned in the form on an "envelope" with the data
+    payload of one or more NHL results to be found in ['results'] list; otherwise, response
+    object body is returned as a single dictionary representation of the results.
+
+    Parameters:
+        url (str): a url that specifies the resource.
+        params (dict): optional dictionary of querystring arguments.
+        timeout (int): timeout value in seconds
+
+    Returns:
+        dict: dictionary representation of the decoded JSON.
+    """
+
+    if params:
+        return requests.get(url, params, timeout=timeout).json()
+    else:
+        return requests.get(url, timeout=timeout).json()
+
+
+def read_csv(filepath, encoding='utf-8', newline='', delimiter=','):
+    """
+    Reads a CSV file, parsing row values per the provided delimiter. Returns a list of lists,
+    wherein each nested list represents a single row from the input file.
+
+    WARN: If a byte order mark (BOM) is encountered at the beginning of the first line of decoded
+    text, call < read_csv > and pass 'utf-8-sig' as the < encoding > argument.
+
+    WARN: If newline='' is not specified, newlines '\n' or '\r\n' embedded inside quoted fields
+    may not be interpreted correctly by the csv.reader.
+
+    Parameters:
+        filepath (str): The location of the file to read
+        encoding (str): name of encoding used to decode the file
+        newline (str): specifies replacement value for newline '\n'
+                       or '\r\n' (Windows) character sequences
+        delimiter (str): delimiter that separates the row values
+
+    Returns:
+        list: a list of nested "row" lists
+    """
+
+    with open(filepath, 'r', encoding=encoding, newline=newline) as file_obj:
+        data = []
+        reader = csv.reader(file_obj, delimiter=delimiter)
+        for row in reader:
+            data.append(row)
+
+        return data
+
+
+def read_csv_to_dicts(filepath, encoding='utf-8', newline='', delimiter=','):
+    """Accepts a file path, creates a file object, and returns a list of dictionaries that
+    represent the row values using the cvs.DictReader().
+
+    Parameters:
+        filepath (str): path to file
+        encoding (str): name of encoding used to decode the file
+        newline (str): specifies replacement value for newline '\n'
+                       or '\r\n' (Windows) character sequences
+        delimiter (str): delimiter that separates the row values
+
+    Returns:
+        list: nested dictionaries representing the file contents
+     """
+
+    with open(filepath, 'r', newline=newline, encoding=encoding) as file_obj:
+        data = []
+        reader = csv.DictReader(file_obj, delimiter=delimiter)
+        for line in reader:
+            data.append(line) # OrderedDict()
+            # data.append(dict(line)) # convert OrderedDict() to dict
+
+        return data
+
+
+def read_json(filepath, encoding='utf-8'):
+    """Reads a JSON document, decodes the file content, and returns a list or dictionary if
+    provided with a valid filepath.
+
+    Parameters:
+        filepath (str): path to file
+        encoding (str): name of encoding used to decode the file
+
+    Returns:
+        dict/list: dict or list representations of the decoded JSON document
+    """
+
+    with open(filepath, 'r', encoding=encoding) as file_obj:
+        return json.load(file_obj)
+
+
+def write_json(filepath, data, encoding='utf-8', ensure_ascii=False, indent=2):
+    """Serializes object as JSON. Writes content to the provided filepath.
+
+    Parameters:
+        filepath (str): the path to the file
+        data (dict)/(list): the data to be encoded as JSON and written to the file
+        encoding (str): name of encoding used to encode the file
+        ensure_ascii (str): if False non-ASCII characters are printed as is; otherwise
+                            non-ASCII characters are escaped.
+        indent (int): number of "pretty printed" indention spaces applied to encoded JSON
+
+    Returns:
+        None
+    """
+
+    with open(filepath, 'w', encoding=encoding) as file_obj:
+        json.dump(data, file_obj, ensure_ascii=ensure_ascii, indent=indent)
